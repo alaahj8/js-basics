8b7b1bca1ba2e7a789aad6b1039e452905b82576
alaahj8 <hij.alaa8@gmail.com>
2022-12-07 16:02:22 +0200

    step5

---
+++ Step_5.js
@@ -0,0 +1,6 @@
+function multiplyBy()
+{
+num1 = document.getElementById("first_number").value;
+num2 = document.getElementById("second_number").value;
+document.getElementById("result").innerHTML = num1 * num2;
+}
