d1b353f5a0de04d4b5b23eed5ad73b277251b752
alaahj8 <hij.alaa8@gmail.com>
2022-12-09 00:50:29 +0200

    step1

---
+++ step 1/step_1.js
@@ -0,0 +1,10 @@
+ function bigImg(){
+    const image1 = document.querySelector("#image1");
+    
+    
+  
+    image1.addEventListener('mouseover', function (){
+        image1.src="images/image1_2.jpg"
+        
+    })
+  }
