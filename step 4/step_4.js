da2a5f81e1cea1b4058424ebf35cee3f776769e7
alaahj8 <hij.alaa8@gmail.com>
2022-12-09 01:01:54 +0200

    step4

---
+++ step 4/step_4.js
@@ -0,0 +1,8 @@
+function test() {
+    var x = confirm("Are you sure you want to reset all text?");
+    if (x == true) {
+      document.getElementById("name").value = "";
+      document.getElementById("surname").value = "";
+      document.getElementById("city").value = "";
+    }
+}
